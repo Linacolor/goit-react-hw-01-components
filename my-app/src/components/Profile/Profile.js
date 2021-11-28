53db40ac690000f94244b4159944ceee6361f5e9
Linacolor <annafragile123@gmail.com>
2021-11-28 17:22:06 +0200

    profile styles

--- my-app/src/components/Profile/Profile.js
+++ my-app/src/components/Profile/Profile.js
@@ -1,6 +1,34 @@
-// import s from './Profile.module.css';
+import s from './Profile.module.css';
 
 function Profile() {
-  return <h2> Hello </h2>;
+  return (
+    <div class={s.profile}>
+      <div class={s.description}>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          alt="User avatar"
+          class={s.avatar}
+        />
+        <p class={s.name}>Petra Marica</p>
+        <p class={s.tag}>@pmarica</p>
+        <p class={s.location}>Salvador, Brasil</p>
+      </div>
+
+      <ul class={s.stats}>
+        <li>
+          <span class={s.label}>Followers</span>
+          <span class={s.quantity}>1000</span>
+        </li>
+        <li>
+          <span class={s.label}>Views</span>
+          <span class={s.quantity}>2000</span>
+        </li>
+        <li>
+          <span class={s.label}>Likes</span>
+          <span class={s.quantity}>3000</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 export default Profile;
